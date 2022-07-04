9e0a1aa311221ede4ab0c9f281fc2d7dc1a1802b
woosang0430 <woosang0430@e8ight.co.kr>
2022-07-04 16:44:56 +0900

    🏗️ 기본 architecture setting

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
