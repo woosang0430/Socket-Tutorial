659476b2121686277fe6cffa58ca26e9755d1233
woosang0430 <woosang0430@e8ight.co.kr>
2022-07-05 16:12:17 +0900

    ✨ client 사이의 socket 통신  연결 및 양방향 채팅 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,54 @@
-alert("hi");
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✔");
+});
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.textContent = message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+function makeStringRes(type, payload) {
+  return JSON.stringify({ type, payload });
+}
+
+function handleSubmit(event, formEl) {
+  event.preventDefault();
+  const input = formEl.querySelector("input");
+  const type = formEl === nickForm ? "nick" : "message";
+
+  const responseJson = {
+    type,
+    payload: input.value,
+  };
+
+  socket.send(JSON.stringify(responseJson));
+  input.value = "";
+}
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeStringRes("new_message", input.value));
+  input.value = "";
+}
+
+function handleNicknameSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeStringRes("nickname", input.value));
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleMessageSubmit);
+
+nickForm.addEventListener("submit", handleNicknameSubmit);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import WebSocket from "ws";
 
 const app = express();
 const port = 3000;
@@ -7,8 +8,40 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
+});
+
+const wss = new WebSocket.Server({ server });
+
+function onSocketClose() {
+  console.log("Disconnected from the Brower ❌");
+}
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+
+  socket["nickname"] = "Anonymous";
+
+  console.log("Connected to Browser ✔");
+  socket.on("close", onSocketClose);
+  socket.on("message", (response) => {
+    const object = JSON.parse(response);
+    switch (object.type) {
+      case "new_message":
+        sockets.forEach((aSocket) => {
+          aSocket.send(`${socket.nickname}: ${object.payload}`);
+          console.log(aSocket.nickname);
+        });
+        break;
+      case "nickname":
+        socket["nickname"] = object.payload;
+        break;
+    }
+  });
 });
